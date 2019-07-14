ac3aac66dbc7e823f541aded4f0439f23ab7d582
Michal Ruzicka Ruzicka <37453000+rutuser@users.noreply.github.com>
2019-07-14 18:32:16 +0100

    Update App.jsx

--- assets/src/component/App.jsx
+++ assets/src/component/App.jsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class App extends React.Component {
-    constructor() {
-        super();
-    }
-
-    getUsers() {
-        fetch('http://localhost:1337/users')
-        .then(response => response.json())
-        .then(response => console.log(response));
-    }
-
-
     render() {
         return (
             <div>
                
-               <Button onClick={this.getUsers}>Get users</Button>
+              REACT-SAILS BOILERPLATE
                
             </div>
         )
